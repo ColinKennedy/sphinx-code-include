129723ed6d1c55d0239dd7793f04eac55b617cfb
Colin Kennedy <colinvfx@gmail.com>
2019-10-26 18:04:53 -0700

    Added keywords to setup.py and removed old, unused comments

--- setup.py
+++ setup.py
@@ -61,10 +61,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
         "Topic :: Utilities",
     ],
     project_urls={
@@ -73,16 +69,12 @@ setup(
         "Issue Tracker": "https://github.com/ColinKennedy/sphinx-code-include/issues",
     },
     keywords=[
-        # eg: 'keyword1', 'keyword2', 'keyword3',
+        "Sphinx", "code-include", "source-code", "source code", "include",
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
         read("requirements.txt").splitlines(),
     ],
     extras_require={
-        # eg:
-        #   'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
     },
 )
