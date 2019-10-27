046bdaf1dbc22508fcbc37324ca38609aa435420
Colin Kennedy <colinvfx@gmail.com>
2019-10-26 18:04:38 -0700

    Changed setup.py to include installation requirements

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        read("requirements.txt").splitlines(),
     ],
     extras_require={
         # eg:
